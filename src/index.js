47f5a1282d3cbf6e98b1864fec68c7327a4e51c3
Bipin Kumar Pandey <bipinjsr05@gmail.com>
2021-02-19 11:29:33 +0530

    First Commit 3

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <>
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-  </>
-  ,
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </>,
   document.getElementById("root")
 );
 
